d4915a94b3bf12d45ef209b0f79ebc1f426f03cb
AndreyWeb-ID <andrey.mail.id@yandex.com>
2018-05-20 11:14:27 +0300

    Change equation

--- task_1/equation.rb
+++ task_1/equation.rb
@@ -10,15 +10,15 @@ d = b**2 - 4 * a * c
 if d > 0
   sqrt = Math.sqrt(d)
   puts "D = #{d}"
-  x1 = (-b + sqrt) / 2 * a
-  x2 = (-b - sqrt) / 2 * a
+  x1 = (-b + sqrt) / (2 * a)
+  x2 = (-b - sqrt) / (2 * a)
   puts "x1 = #{x1}"
   puts "x2 = #{x2}"
 elsif d == 0
   x = -b / (2 * a)
   puts "D = #{d}"
   puts "x1 = x2 = #{x}"
-elsif d < 0
+else
   puts "D = #{d}"
   puts "Нет корней"
 end
